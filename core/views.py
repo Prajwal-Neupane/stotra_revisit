42fa3c5f767b9a934c15951fec732c3cf290febe
Bhadrakalii <choudharysaurav214@gmail.com>
2023-06-28 10:55:32 +0545

    first commit

---
+++ core/views.py
@@ -0,0 +1,17 @@
+import json
+
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import View
+
+# Create your views here.
+
+
+class HomeView(View):
+
+    def get(self, request):
+
+        return render(request, 'base.html')
+
+
+
